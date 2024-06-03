312066883cc3766fc09b342b0837f7ea3895ebd2
HUANG Cheng <cheng@duck.com>
2024-06-03 21:25:29 +0800

    Add the finger mechanism to reduce the need of sending same content

--- bin/sync.rs
+++ bin/sync.rs
@@ -1,4 +1,6 @@
+use std::env::current_dir;
 use std::env::var;
+use std::fs::{read_to_string, write};
 
 use dotenvy::dotenv;
 use sqlx::MySqlPool;
@@ -15,6 +17,22 @@ pub async fn main() {
         .expect("Failed to connect to database");
 
     let posts = get_all_posts(&pool).await.expect("Failed to get all posts");
+
+    let finger = current_dir().unwrap().join("finger");
+
+    let hash = read_to_string(&finger).unwrap_or("".to_string());
+
+    let posts_json = serde_json::to_string(&posts).expect("Failed to serialize posts to JSON");
+
+    let md5 = md5::compute(posts_json);
+
+    if format!("{:x}", md5) == hash {
+        println!("No need to sync posts to Algolia");
+
+        return;
+    }
+
+    write(&finger, format!("{:x}", md5)).expect("Failed to write finger");
 
     sync("typecho_posts", &posts)
         .await
